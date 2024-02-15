bff96f53d1b20eb7b1cba9b9157d8f87b067d7bf
Damir Aliyev <damiraliyevcs@gmail.com>
2024-02-15 14:42:32 +0600

    Configure SceneDelegate.

--- SDUMate/SDUMate/AppLifecycle/SceneDelegate.swift
+++ SDUMate/SDUMate/AppLifecycle/SceneDelegate.swift
@@ -6,17 +6,25 @@
 //
 
 import UIKit
+import Swinject
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var assembler = Assembler([DependencyContainerAssembly()])
+    var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let navigationController = CoordinatorNavigationController()
+        navigationController.navigationBar.isHidden = true
+        appCoordinator = AppCoordinator(router: Router(navigationController: navigationController), container: assembler.resolver)
+        appCoordinator?.start()
+        
         window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
-        window?.rootViewController = viewController
     }
 }
 

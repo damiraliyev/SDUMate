f1e4d4b8d7b9386874dff7d1eab16728414f7b30
Damir Aliyev <damiraliyevcs@gmail.com>
2024-03-02 17:00:07 +0500

    Create and make basic config to UserInfoSetupCoordinator.

--- SDUMate/Flows/Authorization/Modules/UserInfo/Coordinator/UserInfoSetupCoordinator.swift
+++ SDUMate/Flows/Authorization/Modules/UserInfo/Coordinator/UserInfoSetupCoordinator.swift
@@ -5,4 +5,19 @@
 //  Created by Damir Aliyev on 02.03.2024.
 //
 
-import Foundation
+import UIKit
+
+protocol IUserInfoSetupCoordinator: IBaseCoordinator {
+    var onFlowDidFinish: Completion? { get set }
+}
+
+final class UserInfoSetupCoordinator: BaseCoordinator {
+    var onFlowDidFinish: Completion?
+    
+    private let moduleFactory =  UserInfoSetupModuleFactory()
+    
+    override func start() {
+        let aboutView = moduleFactory.makeAboutView()
+        router.push(aboutView)
+    }
+}

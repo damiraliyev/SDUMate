79eabac4ab42d422b544f85d65b2502145ed8b65
Damir Aliyev <damiraliyevcs@gmail.com>
2024-03-03 16:58:44 +0500

    Basic setup of StudentSignUp module.

---
+++ SDUMate/Flows/Authorization/Modules/SignUp/Modules/StudentSignUp/Presenter/StudentSignUpPresenter.swift
@@ -0,0 +1,23 @@
+//
+//  StudentSignUpPresenter.swift
+//  SDUMate
+//
+//  Created by Damir Aliyev on 03.03.2024.
+//
+
+import Foundation
+
+protocol IStudentSignUpPresenter: AnyObject {
+    
+}
+
+final class StudentSignUpPresenter: IStudentSignUpPresenter {
+    
+    weak var view: IStudentSignUpView?
+    private weak var coordinator: IAuthCoordinator?
+    
+    init(view: IStudentSignUpView, coordinator: IAuthCoordinator) {
+        self.view = view
+        self.coordinator = coordinator
+    }
+}
---
+++ SDUMate/Flows/Authorization/Modules/SignUp/Modules/StudentSignUp/View/StudentSignUpViewController.swift
@@ -0,0 +1,64 @@
+//
+//  StudentSignUpViewController.swift
+//  SDUMate
+//
+//  Created by Damir Aliyev on 03.03.2024.
+//
+
+import UIKit
+
+protocol IStudentSignUpView: Presentable {
+    var presenter: IStudentSignUpPresenter? { get set }
+}
+
+final class StudentSignUpViewController: BaseViewController, IStudentSignUpView {
+    
+    var presenter: IStudentSignUpPresenter?
+    
+    private let navigationBar = SMNavigationBar(title: "") {
+        
+    }
+    
+    private let labelsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private let registerLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .medium36
+        label.text = "Register"
+        return label
+    }()
+    
+    private let createAccountLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lavender
+        label.font = .medium16
+        label.text = "Create you account"
+        return label
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        self.view = AuthView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        view.addSubviews([labelsStackView])
+        labelsStackView.addArrangedSubviews([registerLabel, createAccountLabel])
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}

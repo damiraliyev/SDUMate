4394c572bbdbc8bc0302f4fe37d2f3f095e81dc0
Damir Aliyev <damiraliyevcs@gmail.com>
2024-03-21 05:28:35 +0500

    Configure TabBarCoordinatorFactory.

--- SDUMate/Flows/TabBar/Coordinator/TabBarCoordinatorFactory.swift
+++ SDUMate/Flows/TabBar/Coordinator/TabBarCoordinatorFactory.swift
@@ -23,5 +23,36 @@ final class TarBarCoordinatorFactory {
         return (coordinator, navigationController)
     }
     
+    func makeSessionsModule() -> (coordinator: TababbleCoordinator, module: Presentable) {
+        let navigationController = CoordinatorNavigationController()
+        navigationController.tabBarItem.image = Asset.icTabSessions.image
+        let router = Router(navigationController: navigationController)
+        let coordinator = SessionsCoordinator(router: router, container: container)
+        return (coordinator, navigationController)
+    }
+    
+    func makeNewRequestModule() -> (coordinator: TababbleCoordinator, module: Presentable) {
+        let navigationController = CoordinatorNavigationController()
+        navigationController.tabBarItem.image = Asset.icTabNew.image
+        let router = Router(navigationController: navigationController)
+        let coordinator = NewRequestCoordinator(router: router, container: container)
+        return (coordinator, navigationController)
+    }
+    
+    func makeRatingModule() -> (coordinator: TababbleCoordinator, module: Presentable) {
+        let navigationController = CoordinatorNavigationController()
+        navigationController.tabBarItem.image = Asset.icTabRating.image
+        let router = Router(navigationController: navigationController)
+        let coordinator = RatingCoordinator(router: router, container: container)
+        return (coordinator, navigationController)
+    }
+    
+    func makeSettingsModule() -> (coordinator: TababbleCoordinator, module: Presentable) {
+        let navigationController = CoordinatorNavigationController()
+        navigationController.tabBarItem.image = Asset.icTabProfile.image
+        let router = Router(navigationController: navigationController)
+        let coordinator = SettingsCoordinator(router: router, container: container)
+        return (coordinator, navigationController)
+    }
     
 }

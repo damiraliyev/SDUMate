6afac0dc99d43210ba3f6670bd3497f25896090e
Damir Aliyev <damiraliyevcs@gmail.com>
2024-04-17 03:15:17 +0500

    Configure RatingHeaderView.

--- SDUMate/Flows/Rating/Modules/View/CustomViews/RatingHeaderView.swift
+++ SDUMate/Flows/Rating/Modules/View/CustomViews/RatingHeaderView.swift
@@ -9,21 +9,47 @@ import UIKit
 
 final class RatingHeaderView: UIView {
     
+    let goldContributor = TopContributorView()
+    let silverContributor = TopContributorView()
+    let bronzeContributor = TopContributorView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
-        setupConstraints()
+//        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        setupConstraints()
+    }
+    
     private func setupViews() {
-        
+        backgroundColor = ._110F2F
+        addSubviews([silverContributor, goldContributor, bronzeContributor])
     }
     
     private func setupConstraints() {
-        
+        let width = (frame.width - 16) / 3
+        silverContributor.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.width.equalTo(width)
+            make.height.equalTo(153)
+        }
+        goldContributor.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.equalTo(silverContributor.snp.trailing)
+            make.width.equalTo(width)
+        }
+        bronzeContributor.snp.makeConstraints { make in
+            make.leading.equalTo(goldContributor.snp.trailing)
+            make.bottom.equalToSuperview()
+            make.width.equalTo(width)
+            make.height.equalTo(145)
+        }
     }
 }
